3f9728080246957105da4235b4622e31dc74d0cf
XuHan <1178263698@qq.com>
2023-11-05 16:18:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ num }}</div>
+  <button @click="plus">click</button>
+</template>
+<script setup>
+// import { ref } from 'vue';
+let num = $ref(0);
+const plus = () => {
+  num++;
+};
+</script>
+
+<style scoped></style>
