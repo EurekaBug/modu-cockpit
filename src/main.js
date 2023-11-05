80d4383dbfb045da8a5c3070fda21f97c42cdeeb
XuHan <1178263698@qq.com>
2023-11-05 17:04:16 +0800

    feat:全局过滤器

--- src/main.js
+++ src/main.js
@@ -10,7 +10,11 @@ import router from '@/router/index';
 //混入 -- 抽取公用的实例（操作成功与失败消息提醒内容等）
 import mixin from '@/utils/mixin';
 
+//引入全局过滤器
+import { filters } from '@/utils/filters';
+
 const app = createApp(App);
+app.config.globalProperties.$filters = filters;
 app.use(router);
 app.mixin(mixin);
 app.mount('#app');
