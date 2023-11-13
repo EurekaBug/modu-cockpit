caae46854e9e0d3362c2fe5e704055a6eea3a274
XuHan <1178263698@qq.com>
2023-11-13 16:42:07 +0800

    feat:date组件调整

---
+++ src/components/modules/date.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-date-picker v-model="value1" :type="type" :placeholder="placeholder" :size="size" />
+  </div>
+</template>
+<script setup>
+import { getCurrentInstance, defineProps, ref, toRefs, computed } from 'vue';
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'default',
+  },
+  type: {
+    type: String,
+    default: 'date',
+  },
+});
+const { proxy } = getCurrentInstance();
+const value1 = ref('');
+const { size, type } = toRefs(props);
+const placeholder = computed(() => {
+  if (type.value == 'date') {
+    return '请选择日期';
+  } else if (type.value === 'year') {
+    return '请选择年份';
+  } else if (type.value === 'month') {
+    return '请选择月份';
+  } else if (type.value === 'week') {
+    return '请选择周';
+  } else {
+    return '请选择';
+  }
+});
+</script>
+<style lang="scoped"></style>
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,14 +1,53 @@
 <template>
-  <h1>组件测试</h1>
-  <div>
-    <Time />
-    <Time size="large" placeholder="large" />
-    <Time size="small" placeholder="small" />
+  <div class="test">
+    <h1>组件测试</h1>
+    <h2>Date组件</h2>
+    <el-row>
+      <el-col :span="6">
+        <p>设置尺寸大小：</p>
+        <el-radio-group v-model="dateSize" label="size control" size="small">
+          <el-radio-button label="large">large</el-radio-button>
+          <el-radio-button label="default">default</el-radio-button>
+          <el-radio-button label="small">small</el-radio-button>
+        </el-radio-group>
+        <p>设置type：</p>
+        <el-radio-group v-model="dateType" label="size control" size="small">
+          <el-radio-button label="year">year</el-radio-button>
+          <el-radio-button label="month">month</el-radio-button>
+          <el-radio-button label="week">week</el-radio-button>
+          <el-radio-button label="date">date</el-radio-button>
+        </el-radio-group>
+        <!-- <Date size="large" placeholder="large" />
+        <Date size="small" placeholder="small" /> -->
+      </el-col>
+      <el-col :span="6">
+        <p>展示：</p>
+        <Date :size="dateSize" :type="dateType" />
+      </el-col>
+    </el-row>
+    <h2>Table组件</h2>
+    <el-row>
+      <el-col :span="6"
+        ><div />
+        <p>默认：</p>
+        <Table />
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script setup>
-import { getCurrentInstance } from 'vue';
-import Time from '@/components/modules/time.vue';
+import { getCurrentInstance, ref } from 'vue';
+import Date from '@/components/modules/date.vue';
+import Table from '@/components/modules/table.vue';
 const { proxy } = getCurrentInstance();
+const dateSize = ref('default');
+const dateType = ref('date');
 </script>
-<style lang="scoped"></style>
+<style lang="scss" scoped>
+.test {
+  padding: 20px;
+  .el-row {
+    margin-bottom: 20px;
+  }
+}
+</style>
