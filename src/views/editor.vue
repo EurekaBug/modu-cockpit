ca10c75ecb105701b7ac680ccd95d4f8108d3e59
XuHan <1178263698@qq.com>
2024-02-28 16:19:38 +0800

    feat:编辑器布局

---
+++ src/views/editor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <Editor :data="state" />
+  </div>
+</template>
+<script setup>
+import data from '@/components/data.json';
+import Editor from '@/packages/editor';
+const state = $ref(data);
+</script>
+<style scoped lang="scss">
+.app {
+  position: fixed;
+  top: 20px;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
+}
+</style>
