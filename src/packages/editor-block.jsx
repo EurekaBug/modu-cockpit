eb8227d2e8bd15c918174627f5fd2524baa179d0
XuHan <1178263698@qq.com>
2024-03-05 16:19:26 +0800

    feat:菜单组件拖拽

--- src/packages/editor-block.jsx
+++ src/packages/editor-block.jsx
@@ -13,12 +13,23 @@ export default defineComponent({
         }))
         const config = inject('config')
         console.log(config)
+
+        const blockRef = ref(null)
+        onMounted(()=>{
+            let {offsetWidth,offsetHeight} = blockRef.value
+            if(props.block.alignCenter){//只有松手的时候才会居中
+                props.block.left -= offsetWidth/2
+                props.block.top -= offsetHeight/2
+                props.block.alignCenter = false
+            }
+        })
+
         return ()=>{
             //获取组件
             const component = config.componentMap[props.block.key]
             //渲染组件
             const renderComponent = component.render()
-            return <div class="editor-block" style= {blockStyle.value}>
+            return <div class="editor-block" style= {blockStyle.value} ref={blockRef}>
                 {renderComponent}
             </div>
         }
--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -1,15 +1,23 @@
 import {computed,defineComponent} from 'vue'
 import './editor.scss'
 import EditorBlock from './editor-block'
+import deepcopy from 'deepcopy'
+import {useMenuDragger} from './useMenuDragger'
 export default defineComponent({
     props: {
         modelValue:{type:Object}
     },
-    setup(props){
+    emits:['update:modelValue'],//触发事件
+    setup(props,cts){
         const data = computed({
             get(){
                 return props.modelValue
+            },
+            set(val){
+                // console.log(val)
+                cts.emit('update:modelValue',deepcopy(val))
             }
+            
         })
         // console.log(data.value)
 
@@ -18,12 +26,21 @@ export default defineComponent({
             height: data.value.contaniner.height + 'px'
         }))
         const config = inject('config')
+
+        /* 拖拽实现 */
+        const containerRef = ref(null)
+        const {dragstart,dragend} = useMenuDragger(containerRef,data)//拖拽实现
         return ()=>(
             <div class="editor">
                 <div class="editor-left">
-                    {/* 根据注册列表 渲染对应内容 */}
+                    {/* 根据注册列表 渲染对应内容 可是实现h5的拖拽*/}
                     {config.componentList.map(component=>(
-                        <div class="editor-left-item">
+                        <div 
+                            class="editor-left-item" 
+                            draggable
+                            onDragstart={e=>dragstart(e,component)}
+                            onDragend={dragend}
+                        >
                             <span>{component.label}</span>
                             <div>{component.preview()}</div>
                         </div>
@@ -35,7 +52,7 @@ export default defineComponent({
                     {/* 负责产生滚动条 */}
                     <div class="editor-container-canvas">
                         {/* 产生内容区域 */}
-                        <div class="editor-container-canvas__content" style={containerStyle.value}>
+                        <div class="editor-container-canvas__content" style={containerStyle.value} ref={containerRef}>
                             {
                                 (data.value.blocks || []).map((block,index)=>(
                                     <EditorBlock block={block}></EditorBlock>
