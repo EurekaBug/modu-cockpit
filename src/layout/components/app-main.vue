5100f36318d866f7ccd0528ede22f5cd5ad4ab88
XuHan <1178263698@qq.com>
2024-03-17 15:23:42 +0800

    fix:重复渲染

--- src/layout/components/app-main.vue
+++ src/layout/components/app-main.vue
@@ -1,21 +1,21 @@
 <template>
-  <el-scrollbar>
-    <div :style="{ height: height, width: width }">
-      <!-- 路由视图 -->
-      <router-view />
-    </div>
-  </el-scrollbar>
+    <el-scrollbar>
+        <div :style="{ height: height, width: width }">
+            <!-- 路由视图 -->
+            <!-- <router-view /> -->
+        </div>
+    </el-scrollbar>
 </template>
 <script setup>
 defineProps({
-  height: {
-    type: String,
-    default: '',
-  },
-  width: {
-    type: String,
-    default: '',
-  },
+    height: {
+        type: String,
+        default: '',
+    },
+    width: {
+        type: String,
+        default: '',
+    },
 });
 </script>
 <style lang="scss" scoped></style>
